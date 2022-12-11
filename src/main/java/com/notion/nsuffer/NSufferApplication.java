840918ca208a34d737d24abe8d0e4d3a475b4a1b
jeongjuryu <totw2018@naver.com>
2022-12-11 21:05:23 +0900

    Initial commit

---
+++ src/main/java/com/notion/nsuffer/NSufferApplication.java
@@ -0,0 +1,13 @@
+package com.notion.nsuffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NSufferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NSufferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/notion/nsuffer/NSufferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notion.nsuffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NSufferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
