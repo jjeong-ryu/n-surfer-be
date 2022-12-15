102df177d0168b815636e52c1d4486236013d883
JeongJuRyu <totw2018@naver.com>
2022-12-15 19:46:25 +0900

    feat: Card 추가 dto 구현

--- src/main/java/com/notion/nsuffer/card/controller/CardController.java
+++ src/main/java/com/notion/nsuffer/card/controller/CardController.java
@@ -2,12 +2,16 @@ package com.notion.nsuffer.card.controller;
 
 import com.notion.nsuffer.card.dto.GetCardDto;
 import com.notion.nsuffer.card.dto.GetCardListDto;
+import com.notion.nsuffer.card.dto.PostCardDto;
 import com.notion.nsuffer.card.service.CardService;
 import com.notion.nsuffer.common.ResponseDto;
 import com.notion.nsuffer.mypage.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,8 +20,9 @@ public class CardController {
     private final CardService cardService;
 
     @PostMapping
-    public ResponseDto<Object> postCard(){
-        return cardService.postCard();
+    public ResponseDto<Object> postCard(@RequestPart("postCard") PostCardDto.Request dto,
+                                        @RequestPart("file") List<MultipartFile> files){
+        return cardService.postCard(dto,files);
     }
     @GetMapping("/list")
     public ResponseDto<GetCardListDto.Response> getCardList(){
--- src/main/java/com/notion/nsuffer/card/service/CardService.java
+++ src/main/java/com/notion/nsuffer/card/service/CardService.java
@@ -2,6 +2,7 @@ package com.notion.nsuffer.card.service;
 
 import com.notion.nsuffer.card.dto.GetCardDto;
 import com.notion.nsuffer.card.dto.GetCardListDto;
+import com.notion.nsuffer.card.dto.PostCardDto;
 import com.notion.nsuffer.card.entity.Card;
 import com.notion.nsuffer.card.exception.CardNotFoundException;
 import com.notion.nsuffer.card.repository.CardRepository;
@@ -9,8 +10,12 @@ import com.notion.nsuffer.common.ResponseCode;
 import com.notion.nsuffer.common.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,8 +24,6 @@ public class CardService {
     public ResponseDto<GetCardDto.Response> getCard(Long cardId){
         // id를 받아 그에 매핑되는 notion card id를 기반으로 request
         Card card = cardRepository.findById(cardId).orElseThrow(CardNotFoundException::new);
-        String notionId = card.getNotionId();
-        WebClient.create().get().uri()
         // 해당 정보를 받아 최종적으로 프론트로 전달
         return ResponseDto.<GetCardDto.Response>builder()
                 .responseCode(ResponseCode.GET_CARD_LIST)
@@ -29,6 +32,7 @@ public class CardService {
     }
 
     public ResponseDto<GetCardListDto.Response> getCardList() {
+        List<Card> cards = cardRepository.findAll();
         // 현재 DB에 저장된 모든 카드 return
         return ResponseDto.<GetCardListDto.Response>builder()
                 .responseCode(ResponseCode.GET_CARD_LIST)
@@ -36,7 +40,7 @@ public class CardService {
                 .build();
     }
 
-    public ResponseDto<Object> postCard(){
+    public ResponseDto<Object> postCard(PostCardDto.Request dto, List<MultipartFile> files){
         // 먼저 노션에 card post 요청 보냄
         // 이 후, 해당 id를 받아 백엔드 DB에 저장
         return ResponseDto.builder()
