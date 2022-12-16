72851e5b104b1914b2bae11335bc129383a086ac
JeongJuRyu <totw2018@naver.com>
2022-12-16 17:07:15 +0900

    feat: 카드 싱크 구현

--- src/main/java/com/notion/nsuffer/card/dto/NotionGetSyncDbDto.java
+++ src/main/java/com/notion/nsuffer/card/dto/NotionGetSyncDbDto.java
@@ -1,10 +1,29 @@
 package com.notion.nsuffer.card.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotionGetSyncDbDto {
     @Getter
     public static class Response {
-
+        List<Result> results = new ArrayList<>();
+        @Getter
+        public static class Result {
+            private String cover;
+            private Properties properties;
+            @Getter
+            public static class Properties {
+                @JsonProperty("Label")
+                private Label label;
+                @Getter
+                public static class Label {
+                    private String type;
+                }
+            }
+        }
     }
 }
--- src/main/java/com/notion/nsuffer/card/service/NotionCardService.java
+++ src/main/java/com/notion/nsuffer/card/service/NotionCardService.java
@@ -2,9 +2,12 @@ package com.notion.nsuffer.card.service;
 
 import com.notion.nsuffer.card.dto.NotionGetSyncDbDto;
 import com.notion.nsuffer.card.repository.CardRepository;
+import com.notion.nsuffer.common.ResponseCode;
 import com.notion.nsuffer.common.ResponseDto;
 import com.notion.nsuffer.mypage.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -16,18 +19,25 @@ public class NotionCardService {
     private final String NOTION_URL = "https://api.notion.com/v1/databases/";
     private final String VERSION = "2022-06-28";
     public ResponseDto<Object> syncWithNotionDB(User user){
-        String apiKey = user.getNotionApiKey();
-        String dbId = user.getNotionDbId();
-
+//        String apiKey = user.getNotionApiKey();
+        String apiKey = "secret_bGUWaYI7PCpufgCmO7vX4pFD3Qt1kT2qiCv1aAJhcxR";
+//        String dbId = user.getNotionDbId();
+        String dbId = "05e61a0a-a980-4d4d-b5a4-4f73a197ea7d";
         WebClient webClient = WebClient.builder()
-                .baseUrl(NOTION_URL + dbId)
+                .baseUrl(NOTION_URL + dbId + "/query")
                 .defaultHeader("Notion-Version", "2022-06-28")
                 .defaultHeader("Authorization", "Bearer " + apiKey)
                 .build();
-        Mono<NotionGetSyncDbDto.Response> result = webClient.get().retrieve().bodyToMono(NotionGetSyncDbDto.Response.class);
-        result.block();
-
+        NotionGetSyncDbDto.Response result = webClient.post()
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(NotionGetSyncDbDto.Response.class)
+                .block();
+        System.out.println(result.getResults().get(0).getProperties().getLabel().getType());
         // 해당 정보를 우리의 DB에 저장
-        return ResponseDto.builder().build();
+        return ResponseDto.builder()
+                .responseCode(ResponseCode.DB_SYNC)
+                .data(null)
+                .build();
     }
 }
