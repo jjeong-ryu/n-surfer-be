f4b7b16b130ffa28e1af680dd38bf0ee9a2dd2db
jeongjuryu <totw2018@naver.com>
2023-02-20 21:02:36 +0900

    refactor: 중복코드 제거

--- src/main/java/com/notion/nsurfer/auth/service/AuthService.java
+++ src/main/java/com/notion/nsurfer/auth/service/AuthService.java
@@ -133,6 +133,10 @@ public class AuthService {
         String emailAndProvider = verifyResult.getEmailAndProvider();
         String newAccessToken = makeNewAccessToken(emailAndProvider);
         String newRefreshToken = makeNewRefreshToken(emailAndProvider);
+        User user = getUserFromEmailAndProvider(verifyResult.getEmailAndProvider());
+        UserLoginInfo userLoginInfo = getUserInfoFromUser(user);
+        userLoginInfo.updateAccessToken(newAccessToken);
+        userLoginInfo.updateRefreshToken(newRefreshToken);
         return ResponseDto.<ReissueAccessAndRefreshTokenDto.Response>builder()
                 .responseCode(ResponseCode.MAKE_NEW_ACCESS_AND_REFRESH_TOKEN)
                 .data(ReissueAccessAndRefreshTokenDto.Response.builder()
@@ -142,21 +146,15 @@ public class AuthService {
                 .build();
     }
 
-    private String makeNewAccessToken(String emailAndProvider){
+    private String makeNewAccessToken(final String emailAndProvider){
         User user = getUserFromEmailAndProvider(emailAndProvider);
         String accessToken = JwtUtil.createAccessToken(user);
-        UserLoginInfo userLoginInfo = userLoginInfoRepository.findByUser(user)
-                .orElseThrow(UserNotFoundException::new);
-        userLoginInfo.updateAccessToken(accessToken);
         return accessToken;
     }
 
     private String makeNewRefreshToken(String emailAndProvider){
         User user = getUserFromEmailAndProvider(emailAndProvider);
         String refreshToken = JwtUtil.createRefreshToken(user);
-        UserLoginInfo userLoginInfo = userLoginInfoRepository.findByUser(user)
-                .orElseThrow(UserNotFoundException::new);
-        userLoginInfo.updateRefreshToken(refreshToken);
         return refreshToken;
     }
     private User getUserFromEmailAndProvider(String emailAndProvider){
