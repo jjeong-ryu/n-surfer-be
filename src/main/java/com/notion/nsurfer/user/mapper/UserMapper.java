01075c59a63c435b8a2e401c467396f764af0783
jeongjuryu <totw2018@naver.com>
2023-02-20 22:17:53 +0900

    refactor: USER entity 변경

--- src/main/java/com/notion/nsurfer/user/mapper/UserMapper.java
+++ src/main/java/com/notion/nsurfer/user/mapper/UserMapper.java
@@ -18,8 +18,8 @@ public interface UserMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "birthday", ignore = true)
     @Mapping(target = "password", ignore = true)
-    @Mapping(target = "waves", ignore = true)
     @Mapping(target = "isDeleted", ignore = true)
+    @Mapping(target = "cards", ignore = true)
     User signUpToUser(SignUpDto.Request request);
     @Mapping(target = "nickname", source = "response.kakaoAccount.profile.nickname")
     @Mapping(target = "email", source = "response.kakaoAccount.email")
@@ -35,28 +35,10 @@ public interface UserMapper {
     @Mapping(target = "userBirth", source = "user.birthday")
     @Mapping(target = "userType", source = "user.authority")
     @Mapping(target = "imgUrl", source = "user.thumbnailImageUrl")
-    @Mapping(target = "waves", source = "user.waves")
     GetUserProfileDto.Response getUserProfileToResponse(User user);
 //    @Mapping(target = "nickname", ignore = true)
 //    SignUpDto.Request signUpGoogleToRequest(AuthKakaoLoginProfileDto.Response response);
 
-    default List<GetUserProfileDto.Response.Wave> getUserProfileToWaves(List<Wave> waves){
-        List<GetUserProfileDto.Response.Wave> waveList = new ArrayList<>();
-        Map<LocalDate, Integer> waveMap = new HashMap<>();
-        for(Wave wave : waves){
-            LocalDate waveDate = wave.getDate();
-            if(waveMap.containsKey(waveDate)){
-                waveMap.put(wave.getDate(), waveMap.get(waveDate)+1);
-            } else {
-                waveMap.put(waveDate, 1);
-            }
-        }
-        for(Map.Entry<LocalDate, Integer> entry : waveMap.entrySet()){
-            waveList.add(this.getUserProfileToWave(entry.getKey(), entry.getValue()));
-        }
-        return waveList;
-    }
-    GetUserProfileDto.Response.Wave getUserProfileToWave(LocalDate date, Integer count);
     @Mapping(target = "userId", source = "user.id")
     DeleteUserDto.Response deleteUserToResponse(User user);
 }
