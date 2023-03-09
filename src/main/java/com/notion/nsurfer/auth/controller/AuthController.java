401ffdc49769e4d76fe18200ed88806ba9c16d87
jeongjuryu <totw2018@naver.com>
2023-03-09 22:51:41 +0900

    feat: access-refresh 발급 API 제거

--- src/main/java/com/notion/nsurfer/auth/controller/AuthController.java
+++ src/main/java/com/notion/nsurfer/auth/controller/AuthController.java
@@ -1,7 +1,6 @@
 package com.notion.nsurfer.auth.controller;
 
 import com.notion.nsurfer.auth.dto.ReissueAccessTokenDto;
-import com.notion.nsurfer.auth.dto.ReissueAccessAndRefreshTokenDto;
 import com.notion.nsurfer.auth.service.AuthService;
 import com.notion.nsurfer.auth.dto.AuthKakaoLoginDto;
 import com.notion.nsurfer.common.ResponseDto;
@@ -24,16 +23,9 @@ public class AuthController {
     public ResponseEntity<ResponseDto<ReissueAccessTokenDto.Response>> reissueAccessToken(
             HttpServletRequest request
     ) throws IOException {
-        System.out.println(request.getHeader("Authorization"));
         return new ResponseEntity(authService.reissueAccessToken(request), OK);
     }
 
-//    @GetMapping("/reissue/access-refresh-token")
-//    public ResponseEntity<ResponseDto<ReissueAccessAndRefreshTokenDto.Response>> reissueAccessAndRefreshToken(
-//            HttpServletRequest request
-//    ){
-//        return new ResponseEntity(authService.reissueAccessAndRefreshToken(request), OK);
-//    }
     @GetMapping("/login/kakao")
     public ResponseEntity<ResponseDto<AuthKakaoLoginDto.Response>> kakaoLogin(@RequestParam String code,
                                                                               @RequestParam String redirectUrl){
