b4a5ec042a8bbd42cbd95a05b15fcfa9cbb4f82c
jeongjuryu <totw2018@naver.com>
2023-03-22 22:37:23 +0900

    feat: 카드 수정 dto 추가

--- src/main/java/com/notion/nsurfer/card/dto/UpdateCardDto.java
+++ src/main/java/com/notion/nsurfer/card/dto/UpdateCardDto.java
@@ -2,24 +2,35 @@ package com.notion.nsurfer.card.dto;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UpdateCardDto {
     @Getter
+    @Builder
     public static class Request {
-        private String name;
+        private String cardId;
+        private String title;
         private String content;
-        private List<Label> label;
+        private List<Label> labels;
+        @Getter
+        @Builder
         public static class Label {
             private String name;
             private String color;
         }
+        @Builder.Default
+        private List<String> deletedImages = new ArrayList<>();
     }
     @Getter
     @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
     public static class Response {
         @JsonProperty("id")
         private String cardId;
--- src/main/java/com/notion/nsurfer/card/dto/UpdateCardToNotionDto.java
+++ src/main/java/com/notion/nsurfer/card/dto/UpdateCardToNotionDto.java
@@ -1,8 +1,13 @@
 package com.notion.nsurfer.card.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 public class UpdateCardToNotionDto {
 
@@ -10,7 +15,160 @@ public class UpdateCardToNotionDto {
     @Builder
     public static class Request {
         @Builder.Default
-        private Boolean archived = true;
+        private Boolean archived = false;
+
+        @Getter
+        @Builder
+        @AllArgsConstructor
+        @NoArgsConstructor
+        public static class Properties {
+            @JsonProperty("Name")
+            private Name name;
+
+            @Getter
+            @Builder
+            public static class Name {
+                private List<Title> title;
+
+                @Getter
+                @Builder
+                @AllArgsConstructor
+                @NoArgsConstructor
+                public static class Title {
+                    private Text text;
+
+                    @Getter
+                    @Builder
+                    @AllArgsConstructor
+                    @NoArgsConstructor
+                    public static class Text {
+                        private String content;
+                    }
+                }
+            }
+
+            @JsonProperty("Label")
+            private Label label;
+
+            @Getter
+            @Builder
+            public static class Label {
+                @Builder.Default
+                private String type = "multi_select";
+                @JsonProperty("multi_select")
+                private List<MultiSelect> multiSelect;
+
+                @Getter
+                @Builder
+                public static class MultiSelect {
+                    private String name;
+                    private String color;
+                }
+            }
+
+            @JsonProperty("Content")
+            private Content content;
+
+            @Getter
+            @Builder
+            public static class Content {
+                @Builder.Default
+                private String type = "rich_text";
+                @JsonProperty("rich_text")
+                private List<RichText> richTexts;
+
+                @Getter
+                @Builder
+                public static class RichText {
+                    @Builder.Default
+                    private String type = "text";
+                    private Text text;
+
+                    @Getter
+                    @Builder
+                    public static class Text {
+                        private String content;
+                        private Link link;
+
+                        @Getter
+                        @Builder
+                        @AllArgsConstructor
+                        @NoArgsConstructor
+
+                        public static class Link {
+                            private String url;
+                        }
+                    }
+                }
+            }
+
+            @JsonProperty("Creator")
+            private PostCardToNotionDto.Request.Properties.Creator creator;
+
+            @Getter
+            @Builder
+            @AllArgsConstructor
+            @NoArgsConstructor
+            public static class Creator {
+                @Builder.Default
+                private String type = "rich_text";
+                @JsonProperty("rich_text")
+                private List<RichText> richTexts;
+
+                @Getter
+                @Builder
+                public static class RichText {
+                    @Builder.Default
+                    private String type = "text";
+                    private Text text;
+
+                    @Getter
+                    @Builder
+
+                    public static class Text {
+                        private String content;
+                        private Link link;
+
+                        @Getter
+                        @Builder
+                        @AllArgsConstructor
+                        @NoArgsConstructor
+
+                        public static class Link {
+                            private String url;
+                        }
+                    }
+                }
+
+            }
+
+            private PostCardToNotionDto.Request.Properties.File file;
+
+            @Getter
+            @Builder
+            public static class File {
+                @Builder.Default
+                private String type = "files";
+                private List<ImageFile> files;
+
+                @Getter
+                @Builder
+                public static class ImageFile {
+                    private String name;
+                    @Builder.Default
+                    private String type = "external";
+                    private External external;
+
+                    @Getter
+                    @Builder
+                    @AllArgsConstructor
+                    @NoArgsConstructor
+                    public static class External {
+                        private String url;
+                    }
+                }
+            }
+        }
     }
 
     @Getter
